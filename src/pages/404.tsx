1ea0b5e7fb44137e1c62863f610a36310c3f2f83
Maks <semenov@mail.ru>
2022-03-23 20:26:50 +0400

    type для тип схемы в таблице

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-export function Page_404(){
+import React, {useEffect, useState} from "react";
+import {useActions} from "../hook/use-actions";
+import {useTypeSelector} from "../hook/use-typed-selector";
+import {Table} from "../components/table/table";
 
-    return(
+export function Page_404(){
+    const {CreateComponents, DataSaveTable} = useActions();
+    useEffect(()=>{
+        console.log('куку!')
+        CreateComponents(1);
+        CreateComponents(2);
+        DataSaveTable(1);
+    }, []);
+    const  components = useTypeSelector(state => state.components.components);
+    console.log(components)
+    if (components[1]?.data){
+        return(
+            <>
+                <div>404</div>
+                <Table id={1}/>
+            </>
+        )
+    }
+    return  (
         <>
             <div>404</div>
         </>
     )
+
 }
 export default Page_404;
