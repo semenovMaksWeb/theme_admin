07edcd1dd058684979cc65f65b21e0459f7fb97c
Maks <semenov@mail.ru>
2022-03-23 19:55:58 +0400

    обработка js и css ширины ячеек

--- src/components/table/lib/create-dom/create-content.tsx
+++ src/components/table/lib/create-dom/create-content.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 
+
+function style_all_result(elem:any){
+    return {
+        "--width__elem": `${elem.w}px`
+    } as React.CSSProperties;
+}
+
 export function createContent(table:any){
     const body:any = [];
     const header:any = [];
     for (const key in table.schema) {
         const elem = table.schema[key];
-        header.push(<div key={elem.key} className="table__th table_elem">
-            <div className="">{elem.title}</div>
+        const  style = style_all_result(elem);
+        header.push(<div key={elem.key} className="table__th table_elem" style={style}>
+            {elem.title}
         </div>);
     }
 
 
     for (const dataset of table.data){
         const row = [];
+
         for (const key in table.schema) {
             const elem = table.schema[key];
-            row.push(<div key={elem.key} className="table__td table_elem" >{dataset[key]}</div>);
+            const  style = style_all_result(elem);
+            row.push(<div key={elem.key} className="table__td table_elem" style={style}>{dataset[key]}</div>);
         }
         body.push(<div key={dataset.id} className="table__tr">{row}</div>);
     }
