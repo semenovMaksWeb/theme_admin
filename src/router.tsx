ceac8a7a2102a4d77e614a9a46f55bc9b4e91621
Maks <semenov@mail.ru>
2022-03-26 23:21:10 +0400

    router-screen

--- src/router.tsx
+++ src/router.tsx
@@ -4,11 +4,13 @@ import {
     Route,
 } from "react-router-dom";
 const Page_404 = lazy(() => import("./pages/404"))
+const Page_screen = lazy(() => import("./pages/screen"))
 export  const  RouteConfig:React.SFC = () =>{
     return(
         <Suspense fallback={<p>Ожидания загрузки</p>} >
                 <Switch>
-                    <Route  exact path="/" component={Page_404}/>
+                    <Route  exact path="/screen/:id" component={Page_screen}/>
+                    <Route  exact path="*" component={Page_404}/>
                 </Switch>
 
         </Suspense>
