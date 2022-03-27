8bbcee332a12eb0f73fd3a94b5a8d83bdd29b948
Maks <semenov@mail.ru>
2022-03-27 13:42:07 +0400

    конфиг 2 скрина изменения тем форма

---
+++ src/api/config/config_component/id-6.ts
@@ -0,0 +1,107 @@
+export const id_6 = {
+     id:6,
+     style:{
+       order: 5,
+     },
+     type: "form",
+     screen_visible:true,
+     name: "",
+     values:{
+          name_theme: "",
+          description_theme: "",
+     },
+     schema:[
+          {
+               order: 1,
+               type: "fieldset",
+               id:"main_fieldset",
+               legend:{
+                    text: "Изменить тему",
+               },
+               children:[
+                    {
+                         order: 1,
+                         id: "name_theme",
+                         val_type: "string",
+                         type: "input",
+                         label: {
+                              text: "Название темы"
+                         }
+                    },
+                    {
+                         order: 2,
+                         id: "description_theme",
+                         val_type: "string",
+                         type: "input",
+                         label:{
+                              text: "Описание темы"
+                         }
+                    },
+                    {
+                         id: 'update',
+                         order: 3,
+                         key: "button_save",
+                         type: "button",
+                         text: "сохранить",
+                         event:{
+                              click:[
+                                   {
+                                        name: "api",
+                                        params:{
+                                             url:"theme/save",
+                                             config:{
+                                                  name: {
+                                                       result: "body",
+                                                       type:2,
+                                                       id:4,
+                                                       name: 'name_theme'
+                                                  },
+                                                  description: {
+                                                       result: "body",
+                                                       type:2,
+                                                       id:4,
+                                                       name: 'description_theme'
+                                                  }
+                                             }
+                                        }
+                                   },
+                                   {
+                                        name: "reset_values_form",
+                                        params: {
+                                             id: 4
+                                        }
+                                   },
+                                   {
+                                        name: "api",
+                                        params:{
+                                             url:"theme/get_id",
+                                             config: {
+                                                  id: {
+                                                       result: "params",
+                                                       type:1,
+                                                       index: 0,
+                                                       name: 'id'
+                                                  },
+                                             }
+                                        }
+                                   },
+                                   {
+                                        name: "add_table_row",
+                                        params: {
+                                             config: {
+                                                  data:{
+                                                       result: "all",
+                                                       type: 1,
+                                                       index: 2,
+                                                  }
+                                             },
+                                             id: 1,
+                                        }
+                                   }
+                              ]
+                         }
+                    }
+               ]
+          }
+     ]
+}
---
+++ src/api/config/config_screen/screen_id_2.ts
@@ -0,0 +1,5 @@
+import {id_6} from "../config_component/id-6";
+
+export const screen_id_2= {
+    6: id_6,
+}
