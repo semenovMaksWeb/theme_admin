9fe7cec23628f5091b03544dc5d67fd74b2bf962
Maks <semenov@mail.ru>
2022-04-04 19:15:18 +0400

    исключение если api отвалился

--- src/api/config/config_screen/index.ts
+++ src/api/config/config_screen/index.ts
@@ -1,7 +1,9 @@
 import {screen_id_1} from "./screen_id_1";
 import {screen_id_2} from "./screen_id_2";
+import {screen_id_3} from "./screen_id_3";
 
 export const config_screen:any = {
     1: screen_id_1,
-    2: screen_id_2
+    2: screen_id_2,
+    3:screen_id_3,
 }
---
+++ src/api/config/config_screen/screen_id_3.ts
@@ -0,0 +1,3 @@
+export const screen_id_3= {
+
+}
