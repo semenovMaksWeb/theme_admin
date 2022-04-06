c8c512257195e00602b629ead3afff9af8009c31
Maks <semenov@mail.ru>
2022-04-07 01:01:07 +0400

    generator api fix

--- src/api/generatorUrlApi.ts
+++ src/api/generatorUrlApi.ts
@@ -15,16 +15,16 @@ export  async function generatorUrlApi(config: TypeApi, params: any, body: any){
         url = `${apiUrl}/${config.url}`;
     }
     if (config.type === TypeApiMethods.get){
-        return baseApi(await axios.get(url, {params:{...params}} ) );
+        return baseApi(await axios.get(url, {params: params } ) );
     }
     if (config.type === TypeApiMethods.post){
-        return baseApi(await axios.post(url, {...body}, {params:{...params}} ) );
+        return baseApi(await axios.post(url, body, {params: params}  ) );
     }
     if (config.type === TypeApiMethods.delete){
-        return baseApi(await axios.delete(url, {params:{...params}, data:{...body}} ) );
+        return baseApi(await axios.delete(url, {params: params, data: body}  ) );
     }
     if (config.type === TypeApiMethods.put){
-        return baseApi(await axios.put(url, {...body}, {params: {...params}} ) );
+        return baseApi(await axios.put(url, body, {params: params} ) );
     }
     return  null;
 }
